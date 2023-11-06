e7adf40cb70f896db4cbb7dee3121e4df2eb5e3a
sant1dom <santone.domenico2k@gmail.com>
2023-11-06 17:54:06 +0100

    routers, json load, img dynamical

---
+++ routers/site.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+router = APIRouter()
+
+
+templates = Jinja2Templates(directory="static/aroma-master")
+
+@router.get("/", tags=["site"])
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/favourites", tags=["site"])
+def read_root():
+    return FileResponse(path="static/aroma-master/category.html")
+
+
+@router.get("/create", tags=["site"])
+async def create():
+    return FileResponse(path="static/templates/index.html")
+
+
+@router.get("/update", tags=["site"])
+async def update():
+    return FileResponse(path="static/templates/index.html")
+
+
+@router.get("/read", tags=["site"])
+async def read():
+    return FileResponse(path="static/templates/index.html")
+
+
+@router.get("/delete", tags=["site"])
+async def delete():
+    return FileResponse(path="static/templates/index.html")
+
+
+@router.get("/search", tags=["site"])
+async def search():
+    return FileResponse(path="static/templates/index.html")
--- static/aroma-master/js/index-movies.js
+++ static/aroma-master/js/index-movies.js
@@ -1,31 +1,37 @@
 // Store information in localStorage
-document.addEventListener('DOMContentLoaded', async function() {
+document.addEventListener('DOMContentLoaded', async function () {
     const array = [];
     moviesList.innerHTML = '<div class="spinner"></div>'
+    await new Promise((resolve) => setTimeout(resolve, 500));
     let films = await (await fetch('http://127.0.0.1:8000/movies')).json();
     moviesList.innerHTML = '';
     films.forEach(movie => {
-       const movieElement =  `<div class="col-md-6 col-lg-4 col-xl-3" id="${movie.id}">
-                                                    <div class="card text-center card-product">
-                                                        <div class="card-product__img">
-                                                            <img class="card-img" src="${movie.imdb_image}" alt="">
-                                                            <ul class="card-product__imgOverlay">
-                                                            <li><button><i class="ti-search"></i></button></li>
-                                                            <li><button><i class="ti-shopping-cart"></i></button></li>
-                                                            <li><button id="cuore1"><i></i></button></li>
-                                                            </ul>
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <p>Beauty</p>
-                                                            <h4 class="card-product__title"><a href="single-product.html">${movie.title}</a></h4>
-                                                            <p class="card-product__price">$150.00</p>
-                                                        </div>
-                                                    </div>
-                                                </div>`;
+        const movieElement = `<div class="col-md-6 col-lg-4 col-xl-3" id="${movie.id}">
+                                        <div class="card text-center card-product">
+                                            <div class="card-product__img">
+                                                <a href="single-product.html">
+                                                    <img class="card-img" src="${movie.imdb_image}" alt="">
+                                                    <ul class="card-product__imgOverlay">
+                                                    <li><button><i class="ti-search"></i></button></li>
+                                                    <li><button><i class="ti-shopping-cart"></i></button></li>
+                                                    <li><button id="cuore1"><i></i></button></li>
+                                                    </ul>
+                                                </a>
+                                            </div>
+                                            <div class="card-body">
+                                                <a href="single-product.html">
+                                                <p>${movie.movie_length} min.</p>
+                                                <h4 class="card-product__title">${movie.title}</h4>
+                                                <p class="card-product__price">${movie.genre}</p>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        </a>
+                                    </div>`;
         moviesList.insertAdjacentHTML('beforeend', movieElement);
     });
 
-    
+
     //Manage the heart icons            
     const elements = document.querySelectorAll('[id^="cuore"]');
     for (let i = 0; i < elements.length; i++) {
@@ -40,37 +46,37 @@ document.addEventListener('DOMContentLoaded', async function() {
 
         const item = localStorage.getItem(key);
         //if (typeof(Storage) !== "undefined") {
-            if (item) {
-                element.classList.add('heart-full');
-            } else {
-                element.classList.add('heart');
+        if (item) {
+            element.classList.add('heart-full');
+        } else {
+            element.classList.add('heart');
+        }
+
+        array[i].addEventListener('click', function () {
+            var element = array[i].querySelector('i');
+
+            var par = element;
+            for (let j = 0; j < 6; j++) {
+                par = par.parentNode;
             }
+            const key = par.id;
 
-            array[i].addEventListener('click', function() {
-                var element = array[i].querySelector('i');
-
-                var par = element;
-                for (let j = 0; j < 6; j++) {
-                    par = par.parentNode;
+            const item = localStorage.getItem(key);
+            // Code for localStorage/sessionStorage
+            if (item) {
+                element.classList.remove('heart-full');
+                element.classList.add('heart');
+                localStorage.removeItem(key);
+                console.log(`Element removed from local storage: ${key}`);
+            } else {
+                element.classList.remove('heart');
+                element.classList.add('heart-full');
+                for (let k = 0; k < films.length; k++) {
+                    if (films[k].id == key)
+                        localStorage.setItem(key, JSON.stringify(films[k]));
                 }
-                const key = par.id;
-
-                const item = localStorage.getItem(key);
-                // Code for localStorage/sessionStorage
-                if (item) {
-                    element.classList.remove('heart-full');
-                    element.classList.add('heart');
-                    localStorage.removeItem(key);
-                    console.log(`Element removed from local storage: ${key}`);
-                } else {
-                    element.classList.remove('heart');
-                    element.classList.add('heart-full');
-                    for (let k = 0; k < films.length; k++) {
-                        if(films[k].id == key)
-                            localStorage.setItem(key, JSON.stringify(films[k]));
-                    }
-                    console.log(`Element added to local storage: ${key}`);
-                }            
-            });
+                console.log(`Element added to local storage: ${key}`);
+            }
+        });
     }
 });
