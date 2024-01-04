7d3b2f3696df2c2ae3560d8c8ca0792e49224495
MatLeo <leoncini.matteo98@gmail.com>
2024-01-04 18:54:38 +0100

    Home second version

--- movies/src/pages/Home.js
+++ movies/src/pages/Home.js
@@ -1,15 +1,29 @@
 import { useEffect, useState } from 'react';
 import api from "../utils/api";
 import Button from '../components/Button';
+import axios from 'axios';
+
+const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
+
+    const fetchMoviePoster = async (IMDBId) => {
+        const response = await axios.get(`http://omdbapi.com/?apikey=${OMDB_API_KEY}&i=${IMDBId}`);
+        return response.data.Poster;
+    };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const res = await api.get("/movies");
-                setMovies(res.data);  
+                const moviesWithPosters = await Promise.all(res.data.map(async (movie) => {
+                    movie.poster = await fetchMoviePoster(movie.imdb_url.split('/')[4]);
+                    return movie;
+                }));
+
+                setMovies(moviesWithPosters);      
+
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
@@ -19,11 +33,11 @@ const Home = () => {
 
     return (
         <div className="mx-auto">
-            <h1 className="text-2xl">Home</h1>
-            <div className="mx-8 grid grid-cols-5 gap-8">
-                {movies.map(movie => (
+            <h1 className="mt-5 mb-5 text-4xl">Trending Films</h1>
+            <div className="mx-8 grid grid-cols-6 gap-8">
+                {movies.map((movie) => (
                     <div key={movie.id} className="rounded-lg bg-sky-100 shadow-2xl">
-                        <figure><img src="https://img.freepik.com/premium-vector/illustration-operator-director-icon-man-with-movie-camera-logo-studio_15870-1299.jpg" alt="Film" /></figure>
+                        <img className="rounded-t-lg w-60 h-80" src={movie.poster} alt="Film"/>
                         <div className="p-4">
                             <h2 className="text-xl mb-2 overflow-hidden whitespace-nowrap overflow-ellipsis">{movie.title}</h2>
                             <p className="text-base">{movie.release_year}</p>
@@ -32,7 +46,8 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                ))}
+                 ))
+                }
             </div>
         </div>
     );
