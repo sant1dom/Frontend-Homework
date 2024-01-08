9a11cac15c20f9b50dd9dc4fc632fd25efe38df2
Mario <mario123456789mario@gmail.com>
2024-01-08 18:24:52 +0100

    Fix

--- movies/src/components/MovieRow.js
+++ movies/src/components/MovieRow.js
@@ -36,7 +36,12 @@ const MovieRow = ({movie}) => {
     }
 
     return (
-        <div id={`movie_row_${movie.id}`} className="mb-3 w-screen-sm mx-auto border-2 border-solid border-blue-700 rounded-xl p-2 max-w-fit">
+        <div id={`movie_row_${movie.id}`}
+             className="mb-3 w-screen-sm mx-auto border-2 border-solid border-blue-700 rounded-xl p-2 max-w-fit">
+
+            <div className="inline-flex text-left text-lg font-normal w-64">
+                {title}
+            </div>
 
             <div className="inline-flex">
                 <ButtonLink
@@ -52,13 +57,7 @@ const MovieRow = ({movie}) => {
                     label="Delete"
                     classes={"bg-red-500 hover:bg-red-600 mr-2 ml-2"}
                 />
-
             </div>
-
-            <div className="inline-flex text-left text-lg font-normal w-64" >
-                {title}
-            </div>
-
         </div>
     )
 }
