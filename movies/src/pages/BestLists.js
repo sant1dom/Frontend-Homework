435f574663f35f11e1c6691fd07c2ffd13a2bf2b
Giomor <giorgio.morico@student.univaq.it>
2024-01-21 15:44:49 +0100

    Fix grafica bestlists

--- movies/src/pages/BestLists.js
+++ movies/src/pages/BestLists.js
@@ -23,20 +23,22 @@ const BestLists = () => {
     }, []);
 
     return(
-        <div className='container'>
-            <h1 className="mt-5 mb-5 text-4xl">Best Lists</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
-                {bestLists.map((list) => (
-                    <div key={Math.random()} className='flex justify-center'>
-                        <Card
-                            classes={" flex flex-col justify-between hover:shadow-2xl transition duration-300 ease-in-out hover:scale-105 cursor-pointer w-60"}
-                            type={"best-lists"}
-                            text={<h2
-                                className="text-xl mb-2 overflow-hidden whitespace-nowrap overflow-ellipsis">{list.name}</h2>
-                            }
-                            element={list}/>
-                    </div>
-                    ))}
+        <div className='mx-auto'>
+            <div className="mx-8 mb-5">
+                <h1 className="mt-5 mb-5 text-4xl">Best Lists</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+                    {bestLists.map((list) => (
+                        <div key={Math.random()} className='flex justify-center'>
+                            <Card
+                                classes={" flex flex-col justify-between hover:shadow-2xl transition duration-300 ease-in-out hover:scale-105 cursor-pointer w-60"}
+                                type={"best-lists"}
+                                text={<h2
+                                    className="text-xl mb-2 overflow-hidden whitespace-nowrap overflow-ellipsis">{list.name}</h2>
+                                }
+                                element={list}/>
+                        </div>
+                        ))}
+                </div>
             </div>
         </div>
     )
